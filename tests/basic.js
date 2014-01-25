a7eb55496cf359c58abcc9384a52315d51f45c96
Kai Richard Koenig <kai.richard.koenig@googlemail.com>
2014-01-25 20:16:11 +0100

    Adding test for window.onerror arity

--- tests/basic.js
+++ tests/basic.js
@@ -1,13 +1,20 @@
 describe('window.onerror', function() {
   var spy;
+  function noop(){}
   beforeEach(function(){
-      spy = jasmine.createSpy('whatAmI')
+      spy = jasmine.createSpy('window_onerror')
       window.onerror = spy
       var synatx_error = document.createElement('script');
       synatx_error.text = "(function() var a = []; b[1]})()"
       document.body.appendChild(synatx_error)
   })
+  afterEach(function() {
+    window.onerror = noop
+  })
   it('should be called when a syntax error happend', function() {
       expect(spy).toHaveBeenCalled();
+  })
+  it('should be called with errmsg, url and lineno', function() {
+      expect(spy).toHaveBeenCalledWith(jasmine.any(String), jasmine.any(String), jasmine.any(Number));
   })
 })
