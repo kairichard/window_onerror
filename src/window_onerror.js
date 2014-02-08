0208208d0265237130bfd9561707779e1dc8143b
Kai Richard Koenig <kai.richard.koenig@googlemail.com>
2014-02-08 12:47:37 +0100

    Addind basic configuration mechanism as well as error handler call through

--- src/window_onerror.js
+++ src/window_onerror.js
@@ -1,14 +1,33 @@
 (function (win, nav, $) {
   'use strict'
-  // Attach to the onerror method
-  window.onerror = function(msg, url, lineno) {
-    var i = new Image()
-    i.src = "here.html"
-      + "?url=" + encodeURIComponent(url)
-      + "&msg=" + encodeURIComponent(msg)
-      + "&lineno=" + encodeURIComponent(lineno)
-    i.className = "window_onerror_report"
-    document.body.appendChild(i)
-  }
-
+  win.ErrorHandler = (function(){
+    var old_error_handler;
+    var reporting_endpoint = '/client_error'
+    function set_reporting_endpoint(e){
+      reporting_endpoint = e
+    }
+    function setup(){
+      old_error_handler = window.onerror
+      window.onerror = function(msg, url, lineno) {
+        if( "function" == typeof old_error_handler ){
+          old_error_handler(msg, url, lineno)
+        }
+        var i = new Image()
+        i.src = reporting_endpoint
+          + "?url=" + encodeURIComponent(url)
+          + "&msg=" + encodeURIComponent(msg)
+          + "&lineno=" + encodeURIComponent(lineno)
+        i.className = "window_onerror_report"
+        document.body.appendChild(i)
+      }
+    }
+    function teardown(){
+      window.onerror = old_error_handler
+    }
+    return {
+      setup: setup,
+      teardown: teardown,
+      set_reporting_endpoint: set_reporting_endpoint
+    }
+  })()
 })(window, navigator);
--- tests/basic.js
+++ tests/basic.js
@@ -1,17 +1,67 @@
-describe('window.onerror', function() {
+function create_synatx_error(){
+    var synatx_error = document.createElement('script');
+    synatx_error.text = "(function() var a = []; b[1]})()"
+    document.body.appendChild(synatx_error)
+}
+
+function teardown(){
+      ErrorHandler.teardown()
+      window.onerror = undefined
+      var images = Sizzle("img.window_onerror_report")
+      for(a in images){
+        images[a].remove()
+      }
+}
+
+function get_parsed_url_of_report_image(){
+      var error_images = Sizzle("img.window_onerror_report")
+      var error_image = error_images[0]
+      expect(error_images.length).toBe(1)
+      return purl(error_image.src)
+}
+describe('Calling thourgh the original window.onerror', function(){
   beforeEach(function(){
-      var synatx_error = document.createElement('script');
-      synatx_error.text = "(function() var a = []; b[1]})()"
-      document.body.appendChild(synatx_error)
+      spy = jasmine.createSpy('old error handler')
+      window.onerror = spy
+      ErrorHandler.setup()
+      create_synatx_error()
   })
-  xit('should call any original onerror handler', function() {
+
+  afterEach(function(){
+    teardown()
+  })
+
+  it('should call any original onerror handler', function() {
+    expect(spy).toHaveBeenCalled();
+  })
+})
+
+describe('Configurablitiy', function() {
+  beforeEach(function(){
+      ErrorHandler.set_reporting_endpoint("http://error.io/error")
+      ErrorHandler.setup()
+      create_synatx_error()
+  })
+
+  it('should be configurable to report to any endpoint', function() {
+      var param = get_parsed_url_of_report_image()
+      expect(param.data.attr['base']).toBe('http://error.io')
+      expect(param.data.attr['path']).toBe('/error')
+  })
+})
+
+describe('Reporting on syntax error', function() {
+  beforeEach(function(){
+      ErrorHandler.setup()
+      create_synatx_error()
+  })
+
+  afterEach(function(){
+      teardown()
   })
 
   it('should create an image element containing the error msg, url and lineno in the src', function() {
-      var error_images = Sizzle("img.window_onerror_report")
-      var error_image = error_images[0]
-      var param = purl(error_image.src).param()
-      expect(error_images.length).toBe(1)
+      var param = get_parsed_url_of_report_image().param()
       expect(param["msg"]).toContain("SyntaxError:")
       expect(param["lineno"]).not.toBe(undefined)
       expect(param["url"]).not.toBe(undefined)
